a3968199d68ec48d59b54b4d843e57cdbefb4a0b
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2015-05-22 09:16:26 +0900

    once test is js

--- 01.is.js/js/test.js
+++ 01.is.js/js/test.js
@@ -1,11 +1,53 @@
 (function() {
   var foo = 'foo';
+  var isExitElem = document.getElementById('isFooExist');
+  var isNotExitElem = document.getElementById('isFooNotExist');
+  var isNumber1Elem = document.getElementById('isNumber1Even');
+  var isNumber2Elem = document.getElementById('isNumber2Even');
 
-  if(is.existy(foo)) {
-    console.log('`foo` is exist!');
+  var numbers1 = [2, 4, 6, 8];
+  var numbers2 = [2, 4, 5, 8];
+
+
+  /*
+  * 1.変数が存在している（nullやundefinedでない）かどうかのチェック
+  */
+
+  if ( is.existy(foo) ) {
+    isExitElem.innerHTML = '`foo` is exist!';
+
+  } else {
+    isExitElem.innerHTML = '`foo` is not exist!';
   }
-  else {
-    console.log('`foo` is not exist');
+
+  /*
+  * 2.チェック結果がfalseと判定される場合にtrueを返す
+  */
+
+  if ( is.not.existy(foo) ) {
+    isNotExitElem.innerHTML = '`foo` is not exist';
+
+  } else {
+    isNotExitElem.innerHTML = '`foo` is exist!';
+  }
+
+  /*
+  * 3.与えられた複数の値がすべてtrueと評価される場合のみ、trueを返す
+  */
+  if ( is.all.even(numbers1) ) {
+    isNumber1Elem.innerHTML = '`numbers1` is Even!';
+
+  } else {
+    isNumber1Elem.innerHTML = '`numbers1` is not Even!';
+  }
+
+  if ( is.all.even(numbers2) ) {
+    isNumber2Elem.innerHTML = '`numbers2` is Even!';
+
+  } else {
+    isNumber2Elem.innerHTML = '`numbers2` is not Even!';
+
   }
 })();
+
 
